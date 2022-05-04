e594ec3dff4d4e841ea6a53d2cdc4b22fb87fe97
Kevin Rietwyk <krietwyk@gmail.com>
2022-05-04 15:09:26 +1000

    Updated the functions with the modules and added new functions in distances.

--- ray_sphere/distances.py
+++ ray_sphere/distances.py
@@ -4,29 +4,35 @@ def norm_e(v):
     """Euclidean norm of a."""
     return np.dot(v, v)**0.5
 
-def Dist_sph(O, u, P0, r0):
-    """Calculate the possible distances to the sphere for vector."""
+def dist_sph(O, u, P0, r0):
+    """Calculate the possible distances to the sphere for vector, returns
+    Q1, dist1: intersection position and distance in direction of u
+    Q2, dist2: other intersect, may also be in direction of u but greater
+    """
     u = u/np.dot(u, u)**0.5 # Necessary?
-
     k = (np.dot(u, (O-P0))**2 - (np.dot(O-P0, O-P0) - (r0*r0)))**0.5
     if np.isnan(k):
         print("Doesn't intersect with sphere")
+        Q1 = -1
+        Q2 = -1
+        dist1 = -1
+        dist2 = -1
     else: # two options if the photon is in the sphere
         k1 = -np.dot(u, (O-P0)) - k # distance 1
         k2 = -np.dot(u, (O-P0)) + k # distance 2
         # Photon can move in +'ve and -'ve directions of u so assign 
         # v1 and a vector/pos. in direction of u.
-        if np.dot(u, O + k1.u) >= 0: 
+        if np.dot(u, O + k1*u) >= 0: 
             dist1 = k1
             dist2 = k2
-            v1 = O + k1*u
-            v2 = O + k2*u
+            Q1 = O + k1*u
+            Q2 = O + k2*u
         else:
             dist2 = k1
             dist1 = k2
-            v2 = O + k1*u
-            v1 = O + k2*u
-        return (v1, dist1, v2, dist2)
+            Q2 = O + k1*u
+            Q1 = O + k2*u
+        return (Q1, dist1, Q2, dist2)
 
 def line_line_int(P1, v1, P2, v2):
     # vector 1: P1 + t1.v1
@@ -51,23 +57,25 @@ def line_line_int(P1, v1, P2, v2):
         a = a[0]
         
         Q = P1 + a*v1
-        return Q
+        return (Q, a)
 
 def dist_line(O, P, p, u=[]): 
     # If u = [], dist is the shortest distance between O and P, 
     # If u is given, dist is the shortest distance between O and P along u
-    # 
-    dist = -1
-    D = []
+
     if np.dot(O-P, O-P) == 0: 
         # O and P are at the same point, so distace = 0
         dist = 0
         D = O
-    elif len(u) > 0 and np.dot(p, u): # If u vector is given and moving towards line
-        a = np.dot(P - O, p)/(np.dot(p, u))
-        b = norm_e(P - O)
-        dist = (a**2 - b**2)**0.5
-        # Q = O + dist*u
+    elif len(u) > 0:
+        print('using line line interscept')
+        (D, dist) = line_line_int(O, u, P, p)
+        # Below is the old method but the line_line_int function can be used
+    # elif len(u) > 0 and np.dot(p, u): # If u vector is given and moving towards line
+    #     a = np.dot(P - O, p)/(np.dot(p, u))
+    #     b = norm_e(P - O)
+    #     dist = (a**2 - b**2)**0.5
+    #     # Q = O + dist*u
     else:    
         # p = p/np.dot(p, p)**0.5 # ensure unit vector
         c = P + p*2*np.dot((O-P), (O-P))**0.5
@@ -78,6 +86,81 @@ def dist_line(O, P, p, u=[]):
             *np.dot(p, O-P)/(np.dot(p, O-P)*np.dot(p, O-P))**0.5
     return (dist, D)
 
+def chord_dist(O, P, P0, r0):
+    """
+    Calculate the chord distance from photon on sphere to port.
+    O: photon position
+    P: port centre
+    P0: sphere centre
+    r0: sphere radius
+    a: angle between the two points from the centre of the sphere
+    dist: chord distance
+    """
+
+    a = np.arccos(np.dot(O-P0, P-P0) \
+        /(np.dot(O-P0, O-P0)**0.5 * np.dot(P-P0, P-P0)**0.5))
+    # chord = a*R
+    dist = a*r0
+    return dist
+
+def plane_create(P0): 
+    # Creates two orthogonal vectors, penpendicular to P0
+    if (P0==0).all():
+        print('No vector was provided')
+    n = np.array([1, 0, 0])
+    if (P0 == n).all():
+        n = np.array([0, 1, 0])
+    P0 = P0/np.dot(P0, P0)**0.5
+    w2 = np.cross(P0,n)
+    w2 = w2/np.dot(w2, w2)**0.5
+    w1 = np.cross(w2,P0)
+    if np.dot(w1,P0) < 0:
+        w1 = np.cross(-w2,P0)
+    w1 = w1/np.dot(w1, w1)**0.5
+    return w1, w2  
+
+def dist_cylinder(O, u, P, n, r): 
+    # interactions with the cylinder walls not the caps
+    # u is photon, n is port axis from c
+    ## Update variables
+    a = u - n*np.dot(u, n)
+    b = (O-P) - n*np.dot(O-P, n)
+    
+    A = np.dot(a, a)
+    B = 2*np.dot(a, b)
+    C = np.dot(b, b) - r**2
+    
+    # Discriminant
+    delta = B**2 - 4*A*C
+    
+    if delta < 0: 
+        # doesn't interact with port, so create dummies
+        s = -1
+        v1 = -1
+        d = -1
+        v2 = -1
+    else:            
+        # 2 solutions of quadratic formula
+        s1 = (-B + delta**0.5)/(2*A)
+        s2 = (-B - delta**0.5)/(2*A)
+        
+        Q1 = O + s1*u
+        # P2 = O + s2*u
+        
+        # from closest point on cylinder axis to intersection points dot u
+        
+        if np.dot((Q1 - (n*np.dot(Q1-P,n)+P)), u) > 0:
+        # P2 - (n*np.dot(P2-c,n)+c)
+            s = s1
+            v1 = O + s*u
+            d = s2 
+            v2 = O + d*u
+        else:
+            s = s2
+            v1 = O + s*u
+            d = s1 
+            v2 = O + d*u
+    return (s, v1, d, v2)
 
 # def dist_plane(O, u, P0, n):
 #     # Distance to a plane with unit vector n that passes through pt c0
--- ray_sphere/plot_chamber.py
+++ ray_sphere/plot_chamber.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from distances import dist_sph
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from numpy import pi
 
-def Plot_sphere(r0, P0, npts, figno):
+"""
+Capitals define points
+n and u will always be unit vectors
+O and u relate exclusively to photon position and direciton
+n is used for the port axis
+
+P0 and r0 are the sphere centre position and radius
+p: vector from sphere centre
+f and t are used to denote angles phi and theta 
+r - cylinder radius
+h - cylinder length
+P, Q: Points 
+v: vector 
+"""
+
+def plot_sphere(r0, P0, npts, figno):
     """Create datapoints for plotting a sphere, t and f are theta and phi.""" 
     f, t = np.mgrid[0:2*np.pi:npts*1j, 0:np.pi:npts*1j]
     x = r0*np.cos(f)*np.sin(t) + P0[0]
@@ -20,3 +35,63 @@ def Plot_sphere(r0, P0, npts, figno):
     # Add marker in centre
     ax.scatter(P0[0], P0[1], P0[2]) 
     return ax
+
+def plot_cylinder(P, n, r, h, npts, figno, ax):
+    """Create datapoints for plotting a cylinder intersecting the sphere.
+    P - position of port of the cylinder at the sphere
+    n - cylinder axis
+    r - cylinder radius
+    h - cylinder length
+    """
+    ## replace with create plane?
+    # A vector not in the same direction as n
+    k = np.array([1, 0, 0])
+    if (n == k).all():
+        k = np.array([0, 1, 0])
+    # A unit vector n1 perpendicular to n
+    w1 = np.cross(n, k)
+    w1 = w1/np.dot(w1, w1)**0.5
+    # A unit vector perpendicular to v and n1
+    w2 = np.cross(n, w1)
+    w2 = w2/np.dot(w2,w2)
+    # Surface ranges over t from 0 to length of axis and f = 0 to 2*pi
+    f, d = np.mgrid[0:2*np.pi:npts*1j, 0:h:3j]
+    # Generate coordinates for the surface
+    X, Y, Z = [P[i] + n[i]*d + r*np.sin(f)*w1[i] + r*np.cos(f)*w2[i] \
+               for i in [0, 1, 2]]
+    ax.plot_surface(X, Y, Z, color='0', alpha=0.3)
+    return
+
+def plot_cylinder_offaxis(r0, P0, p, n, r, h, w1, w2, npts, figno, ax): 
+    # Example of intersection between cylinder projection and sphere, Q
+    # (v1, dist1, v2, dist2) = Dist_sph(w1*r + r0*p-P0, n, P0, r0)
+  
+    # Plot the intersection between cylinder projection and sphere
+    psi_ = np.linspace(0,2*np.pi,npts)
+    G = np.zeros([3,npts])
+    G2 = np.zeros([3,npts])
+    for i in range(len(psi_)):
+        # Define a vector for the cylinder end then project towards the sphere, G
+        Q = r*(w1*np.cos(psi_[i]) + w2*np.sin(psi_[i])) + r0*p-P0
+        (Q1, *_) = dist_sph(Q, n, P0, r0)
+        
+        # Sphere end of the cylinder
+        G[:,i] = Q1
+        
+        # Far end of the cylinder
+        G2[:,i] = Q + n*h
+        
+
+        if i > 0:
+            x = [G[0, i-1], G2[0, i-1], G2[0,i], G[0,i]]
+            y = [G[1, i-1], G2[1, i-1], G2[1,i], G[1,i]]
+            z = [G[2, i-1], G2[2, i-1], G2[2,i], G[2,i]]
+            verts = [list(zip(x, y, z))]
+            ax.add_collection3d(Poly3DCollection(verts))
+    ax.plot3D(G[0], G[1], G[2], color = 'black')
+    ax.plot3D(G2[0], G2[1], G2[2], color = 'black')
+    
+    ax.scatter(r0*p[0], r0*p[1], r0*p[2]) # centre of port on the sphere
+
+
+
