26f117bd7db10c71c7e0d2733e53dec08f99ced0
Kevin Rietwyk <krietwyk@gmail.com>
2022-05-07 08:58:45 +1000

    Toying with the idea of having default options. This may be a module that users will have to chose to add.

--- ray_sphere/options.py
+++ ray_sphere/options.py
@@ -1,1 +1,12 @@
+from ray_sphere.plot_chamber import plot_sphere
+import numpy as np
+import matplotlib.pyplot as plt
 
+plt.interactive(True)
+
+flag_plot = 1
+
+r0 = 1
+P0 = np.array([0, 0, 0])
+if flag_plot == 1:
+    ax = plot_sphere(r0, P0, 50, 0)
