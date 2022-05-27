4b4cac7ed0960b38745f685cebd0af2d1aba89dc
Kevin Rietwyk <krietwyk@gmail.com>
2022-05-27 11:09:31 +1000

    Adjusted the way plotting of the sphere is initiated. It has been moved to options.

--- ray_sphere/plot_chamber.py
+++ ray_sphere/plot_chamber.py
@@ -18,25 +18,26 @@ P, Q: Points
 v: vector 
 """
 
-def plot_sphere(r0, P0, npts, figno=1):
+def plot_sphere(r0, P0, npts, ax, figno=1):
     """Create datapoints for plotting a sphere, t and f are theta and phi.""" 
     f, t = np.mgrid[0:2*np.pi:npts*1j, 0:np.pi:npts*1j]
     x = r0*np.cos(f)*np.sin(t) + P0[0]
     y = r0*np.sin(f)*np.sin(t) + P0[1]
     z = r0*np.cos(t) + P0[2]
     # Create figure for sphere
-    fig = plt.figure(figno)
-    fig.clf()
-    ax = plt.axes(projection='3d')
+    
+    # fig = plt.figure(figno)
+    # fig.clf()
+    # ax = plt.axes(projection='3d')
     # Turn off grid lines
     ax.grid(False)
     # Draw sphere
     ax.plot_surface(x, y, z, color='0.0', alpha=0.3)    
     # Add marker in centre
     ax.scatter(P0[0], P0[1], P0[2]) 
-    return ax
+    return #ax
 
-def plot_cylinder(P, n, r, h, npts, figno, ax):
+def plot_cylinder(r0, P0, P, n, r, h, npts, figno, ax):
     """Create datapoints for plotting a cylinder intersecting the sphere.
     P - position of port of the cylinder at the sphere
     n - cylinder axis
