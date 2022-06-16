e154c3f591a7675b0d15980ce349b8809ec2a46f
Kevin Rietwyk <krietwyk@gmail.com>
2022-06-16 13:05:47 +1000

    Removed or commented out functions that have been shifted to either iteractions or rt_objects.

--- ray_sphere/locations.py
+++ ray_sphere/locations.py
@@ -5,7 +5,6 @@ from ray_sphere.distances import norm_e, disp_sph, line_line_int, \
 from ray_sphere.plot_chamber import plot_sphere, plot_cylinder, \
     plot_cylinder_offaxis
     
-# from ray_sphere.options import *
 from ray_sphere.options import P0, r0, rhos, ax
 
 def is_in_on(photon, chamber, i):
@@ -96,181 +95,55 @@ def locator(photon, chamber):
             print(f'Outside of chamber {i}')
     return i
 
-def in_sph(photon, chamber): # Det. intersection
-    while  photon.state == 1 and photon.loc == -1:
-        if len(chamber.ele) == 0:
-            print('hits wall')
-            Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
-            if np.random.rand() <= rhos:
-                v, k = lamb_ref(photon.u, Q-P0)
-                photon.old.append(photon.O)
-    
-                ax.plot3D([Q[0], photon.O[0]], 
-                          [Q[1], photon.O[1]], 
-                          [Q[2], photon.O[2]], color = 'black')
-                photon.O = Q
-                photon.u = v
-            else:
-                photon.state = 0
+
+
+
+# def in_sph(photon, chamber): # Det. intersection
+#     while  photon.state == 1 and photon.loc == -1:
+#         if len(chamber.ele) == 0:
+#             print('hits wall')
+#             Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
+#             lamb_event(photon, 1, Q-P0, Q, rhos, 1, color='black')
+            
+#         for i in range(len(chamber.ele)):
+#             if np.dot(chamber.ele[i].n, chamber.ele[i].p) == 1: #on axis
+#                 disp, Q = line_plane_int(photon.O, photon.u, chamber.ele[i].P, 
+#                                          chamber.ele[i].n)
+#                 if disp > 0 and norm_e(Q-chamber.ele[i].P) < chamber.ele[i].r:
+#                     print('run event')
+#                     photon.loc = i
+#                     ax.plot3D([Q[0], photon.O[0]], 
+#                               [Q[1], photon.O[1]], 
+#                               [Q[2], photon.O[2]], color = 'black')
+#                     photon.O = Q
+#                     break
+#                 else:
+#                     print('hits wall')
+#                     Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
+#                     lamb_event(photon, 1, Q-P0, Q, rhos, 1, color='black')
+
+#             elif np.dot(chamber.ele[i].n, chamber.ele[i].p) != 1: # off axis
+#                 # print('off axis')
+#                 (Q, disp1, Q2, disp2) = disp_sph(photon.O, photon.u, P0, r0)
+#                 disp_r, *_ = disp_line_point(chamber.ele[i].P, chamber.ele[i].n, Q)
+#                 if norm_e(disp_r) < chamber.ele[i].r:
+#                     print('run event')
+#                     photon.loc = i
+#                     ax.plot3D([Q[0], photon.O[0]], 
+#                               [Q[1], photon.O[1]], 
+#                               [Q[2], photon.O[2]], color = 'black')
+#                     photon.O = Q
+#                     break
+#                 else: # Doesn't interact with port openings so must hit a wall
+#                     print('hits wall')
+#                     Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
+#                     lamb_event(photon, 1, Q-P0, Q, rhos, 1, color='black') 
+#                 # else: # no interactions with ports, photon hits wall
+#                 #     print('hits wall')
+#                 #     Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
+#                 #     lamb_event(photon, Q-P0, rhos, Q, 1, color='black')
         
-        for i in range(len(chamber.ele)):
-            if np.dot(chamber.ele[i].n, chamber.ele[i].p) == 1:
-                disp, Q = line_plane_int(photon.O, photon.u, chamber.ele[i].P, 
-                                         chamber.ele[i].n)
-                if disp > 0 and norm_e(Q-chamber.ele[i].P) < chamber.ele[i].r:
-                    print('run event')
-                    photon.loc = i
-                    photon.old.append(photon.O)
-                    ax.plot3D([Q[0], photon.O[0]], 
-                              [Q[1], photon.O[1]], 
-                              [Q[2], photon.O[2]], color = 'black')
-                    photon.O = Q
-                    # event(photon, chamber, i, comp_ind, Q)
-                    break
-                else:
-                    print('hits wall')
-                    Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
-                    if np.random.rand() <= rhos:
-                        v, k = lamb_ref(photon.u, Q-P0)
-                        photon.old.append(photon.O)
-                        ax.plot3D([Q[0], photon.O[0]], 
-                                  [Q[1], photon.O[1]], 
-                                  [Q[2], photon.O[2]], color = 'black')
-                        photon.O = Q
-                        photon.u = v
-                    else:
-                        photon.state = 0
-                
-            elif np.dot(chamber.ele[i].n, chamber.ele[i].p) != 1:
-                # print('off axis')
-                (Q, disp1, Q2, disp2) = disp_sph(photon.O, photon.u, P0, r0)
-                disp_r, *_ = disp_line_point(chamber.ele[i].P, chamber.ele[i].n, Q)
-                if norm_e(disp_r) < chamber.ele[i].r:
-                    # event(photon, chamber, i, comp_ind, Q)
-                    photon.loc = i
-                    break
-                else: # no interactions with ports, photon hits wall
-                    print('hits wall')
-                    Q, disp_s, _, _ = disp_sph(photon.O, photon.u, P0, r0)
-                    if np.random.rand() <= rhos:
-                        v, k = lamb_ref(photon.u, Q-P0)
-                        photon.old.append(photon.O)
-    
-                        ax.plot3D([Q[0], photon.O[0]], 
-                                  [Q[1], photon.O[1]], 
-                                  [Q[2], photon.O[2]], color = 'black')
-                        photon.O = Q
-                        photon.u = v
-                    else:
-                        photon.state = 0
-                        print('photon died')
+
                         
-                        
-def event(photon, chamber, i, comp_ind, Q):
-    
-    # if chamber.ele[i].h == 0: # if there is no length, so this is the end cap
-    #     if chamber.ele[i].end_type1 == 0:
-    #         ax.plot3D([Q[0], photon.O[0]], 
-    #                   [Q[1], photon.O[1]], 
-    #                   [Q[2], photon.O[2]], color = 'red')
-    #         photon.state = 0 
-    #         print('photon detected')
-
-    # elif chamber.ele[i].h > 0 and comp_ind == 0:
-    #     photon.old.append(photon.O)
-    #     ax.plot3D([Q[0], photon.O[0]], 
-    #               [Q[1], photon.O[1]], 
-    #               [Q[2], photon.O[2]], color = 'black')
-    #     photon.O = Q
-    #     photon.loc = i
-    
-    # Interaction with cylinder wall, lambertian reflelction
-    if chamber.ele[i].h > 0 and comp_ind == 1:
-        a = chamber.ele[photon.loc]
-        b = np.dot(Q - (a.P+a.h*a.n), -a.n)
-        lamb_event(photon, Q-(a.P+a.h*a.n-b*a.n), Q, rhos, 1, color='black')
-        # if np.random.rand() <= rhos:
-        #     a = chamber.ele[photon.loc]
-        #     b = np.dot(Q - (a.P+a.h*a.n), -a.n)
-        #     v, k = lamb_ref(photon.u, Q - (a.P+a.h*a.n-b*a.n))
-        #     photon.old.append(photon.O)
-
-        #     ax.plot3D([Q[0], photon.O[0]], 
-        #               [Q[1], photon.O[1]], 
-        #               [Q[2], photon.O[2]], color = 'black')
-        #     photon.O = Q
-        #     photon.u = v
-        # else:
-        #     photon.state = 0 
-    
-    # Interaction with port end (away from sphere)        
-    # elif chamber.ele[i].h > 0 and (comp_ind == 2:
-    elif comp_ind == (0 or 2):    
-        # negative rho means it pass through through, if moving outwards
-        dist_c = norm_e(Q - chamber.ele[i].h*chamber.ele[i].n+chamber.ele[i].P)
-        if chamber.ele[i].rho < 0:  
-            if np.dot(photon.u, chamber.ele[i].n) > 0:
-                photon.loc = -2 
-            elif np.dot(photon.u, chamber.ele[i].n) < 0:
-                photon.loc = i
-        # if it interacts with end port check it is within the detector radius
-        elif dist_c <= chamber.ele[i].rd:
-            
-            photon = detect(photon, chamber, i, Q)
-        else: 
-            photon = lamb_event(photon, chamber.ele[i].n, 
-                                              Q, chamber.ele[i].rho, 1, 
-                                              color='black')
-            
-def detect(photon, chamber, element, Q):
-    # NA = ni.asin(theta), ni is refractive index
-    theta = np.acos(np.dot(photon.u, chamber.ele[element].n))
-    acceptance_angle = np.asin(chamber.ele[element].NA/chamber.ele[element].ni)
-    if theta <= acceptance_angle:
-        print('photon detected')
-        ax.plot3D([Q[0], photon.O[0]], 
-                  [Q[1], photon.O[1]], 
-                  [Q[2], photon.O[2]], color = 'red')
-        photon.O = Q
-        photon.state = -1
-        # photon.fate_rec()
-    return photon
-
-def lamb_event(photon, n, rho, Q, flag_plot, color='black'):
-    # flag_new_loc = 0
-    # if rho < 0: # pass through (open port)
-    #     photon.old.append(photon.O)
-    #     photon.O = Q
-    #     flag_new_loc = 1
-    if np.random.rand() <= rho:
-        v, k = lamb_ref(photon.u, n)
-        photon.old.append(photon.O)
-        if flag_plot >= 1:
-            ax.plot3D([Q[0], photon.O[0]], 
-                      [Q[1], photon.O[1]], 
-                      [Q[2], photon.O[2]], color = 'black')
-        photon.O = Q
-        photon.u = v
-    else:
-        photon.state = 0
-    return photon #, flag_new_loc
-
-def lamb_ref(u, n): 
-    # n must be a unit vector but u doesn't need to be.
-    # for u and n pointing in the same directions. 
-    # Resulting vector points opposite to surface normal, -n. Returns unit vector.
-    
-    t = np.pi*np.random.rand() #% acos(1-2*rand);
-    f = 2*np.pi*np.random.rand() #% acos(1-2*rand);
-    st = np.sin(t)
-    
-    # k = np.dot(photon.u, n)*n - photon.u
-    k = -n
-    k = k/norm_e(k)
-    v = [k[0]+st*np.cos(f), k[1]+st*np.sin(f), k[2]+np.cos(t)]
-    v = v/norm_e(v)
-    return v, k
-
-def spec_ref(u, n): 
-    return 2*np.dot(n, u)*n - u
+  
 
