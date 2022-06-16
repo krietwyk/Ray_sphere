1223c813c71b0123030941a42909b51f55a358ea
Kevin Rietwyk <krietwyk@gmail.com>
2022-06-16 13:07:07 +1000

    Improvements to the storage of photon fate information.

--- ray_sphere/photon.py
+++ ray_sphere/photon.py
@@ -1,10 +1,15 @@
-from distances import plane_create
+from distances import plane_create, norm_e
 import numpy as np
 
 class photon_create:
     info = []
     fate = []
     num = 0
+    
+    fate_type_dic = {-2:"not detected", 
+                          -1:"escaped", 
+                          0:"absorbed", 
+                          1:"detected"}
     
     # convenient way to track the details of the photon
     def __init__(self, P, w, sigma_r, dt, state, loc=-99): # o, u, v, state, loc, :  
@@ -14,13 +19,19 @@ class photon_create:
         self.sigma_r = sigma_r
         self.dt = dt
         # self.P = P
-        # self.w = w
-        w1, w2 = plane_create(w)
+        self.w = w/norm_e(w)
+        w1, w2 = plane_create(self.w)
         ### Need to choose if to you gaussian position spread or uniform
         self.O = P + w1*sigma_r*np.random.rand() + w2*sigma_r*np.random.rand()
         # self.o = c0 + w1*np.random.normal(0, sigma_r, 1)+ w2*np.random.normal(0, sigma_r, 1)
-        self.u = w + w1*dt*np.random.rand() + w2*dt*np.random.rand()
-    
+        self.u = self.w + w1*dt*np.random.rand() + w2*dt*np.random.rand()
+        # self.
+        
+        # self.loc_dic = {-2:"Outside", 
+        #                 -1:"Sphere", 
+        #                 0:"", 
+        #                 1:"detected"}
+        
     def __setattr__(self, name, value):
         self.__dict__[name] = value
         try:
@@ -35,11 +46,10 @@ class photon_create:
     def info_empty(self):
         photon_create.info = []
     
-    def fate_rec(self, photon, element, fate_type, angle=np.nan):
-        # fate_type = {-1:"escaped", 0:"absorbed", 1:"detected"}
+    def fate_rec(self, photon, comp, fate_type, angle=np.nan):
         photon_create.fate.append([photon.num, photon.O, photon.u, photon.loc, 
-                                   element, fate_type, angle])
+                                   comp, fate_type, angle])
     
     def fate_empty(self):
-            photon_create.fate = []
+        photon_create.fate = []
         
